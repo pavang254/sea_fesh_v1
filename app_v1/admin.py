1287bbd1900e22dd7916455357932af46f830ac4
Pavan.G <pavan.g@datasemantics.co>
2022-08-01 20:34:32 +0530

    testing models

--- app_v1/admin.py
+++ app_v1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Catgegory)
+admin.site.register(Product)
+admin.site.register(Size_Price)
+admin.site.register(Prod_details)
--- app_v1/models.py
+++ app_v1/models.py
@@ -1,3 +1,37 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+class Catgegory(models.Model):
+    cat_id = models.IntegerField(primary_key=True)
+    cat_name = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.cat_name
+
+class Product(models.Model):
+    prod_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Catgegory, default='Fish', on_delete=models.SET_DEFAULT)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Size_Price(models.Model):
+    prod_id = models.ForeignKey(Product, on_delete=models.CASCADE) 
+    small_price = models.FloatField()
+    med_price = models.FloatField()
+    big_price = models.FloatField()
+
+    def __str__(self) -> str:
+        return f' {self.prod_id} -> small = {self.small_price}, med = {self.med_price}, big = {self.big_price} '
+
+class Prod_details(models.Model):
+    type_clean = models.BooleanField(default=True)
+    price = models.ForeignKey(Size_Price, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.price
+
+
